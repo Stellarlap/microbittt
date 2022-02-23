7c0e5fb0a401840ea4306ebc586d87c24f2be0b6
Stellarlap <100299314+Stellarlap@users.noreply.github.com>
2022-02-23 10:48:52 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Hello")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showArrow(ArrowNames.North)
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+basic.showIcon(IconNames.Ghost)
+basic.showNumber(4)
+basic.showString("Hello!")
+let number = 4
+let hello = 2
